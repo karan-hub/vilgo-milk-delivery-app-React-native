214152e355231682cf815e866f5a3876d5d1bef2
karan <karanc4143@gmail.com>
2026-01-06 13:21:50 +0530

    feat(ui): update banner component

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,7 @@ function Banner() {
   return (
     <View className="mx-4   rounded-3xl overflow-hidden shadow-lg">
       <ImageBackground
-        source={require("@/assets/images/homeBanner v2.png")}
+        source={{uri:"https://res.cloudinary.com/dsykkejzw/image/upload/v1767424252/homeBanner_v2_kmnenr.png"}}
         className="w-full"
         style={{ aspectRatio: 2.4 }} 
         resizeMode="contain"
